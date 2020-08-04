90f88e7c710bf0cc0fa93ed4b17213b7571c82e6
leonardo studart <lstudart@pyx4.com>
2020-08-03 21:30:43 -0400

    feat: add form and inputs inside header

--- src/pages/teacher-list/index.tsx
+++ src/pages/teacher-list/index.tsx
@@ -6,10 +6,31 @@ import PageHeader from '../../components/page-header'
 
 
 
+
+
+
+
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="These are the available Proffies" />
+      <PageHeader title="These are the available Proffies" >
+        <form action="" id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Discipline</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Week day</label>
+            <input type="text" id="week-day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Time</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
     </div>
   )
 }
