3e30780d88a15a246074715315a99b0c8b8982db
leonardo studart <lstudart@pyx4.com>
2020-08-03 21:17:40 -0400

    chore: use PageHeader in teacherForm and teacherList

--- src/pages/teacher-list/index.tsx
+++ src/pages/teacher-list/index.tsx
@@ -1,30 +1,15 @@
 import './styles.css'
 
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-import backIcon from '../../assets/icons/back.svg'
-import logoImg from '../../assets/images/logo.svg'
-
-
-
+import PageHeader from '../../components/page-header'
 
 
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to='/' >
-            <img src={backIcon} alt="go back" />
-          </Link>
-          <img src={logoImg} alt="Proffy" />
-        </div>
-        <div className="header-content">
-          <strong>These are the available Proffys</strong>
-        </div>
-      </header>
+      <PageHeader title="These are the available Proffies" />
     </div>
   )
 }
