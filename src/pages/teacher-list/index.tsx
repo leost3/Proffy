8fedf82f787978394ce77ea999755f6beaebf63a
leonardo studart <lstudart@pyx4.com>
2020-08-03 21:03:02 -0400

    fix: place header-content inside of header

--- src/pages/teacher-list/index.tsx
+++ src/pages/teacher-list/index.tsx
@@ -1,3 +1,5 @@
+import './styles.css'
+
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -6,21 +8,24 @@ import logoImg from '../../assets/images/logo.svg'
 
 
 
+
+
+
 function TeacherList() {
   return (
-   <div id="page-teacher-list" className="container">
-     <header className="page-header">
-       <div className="top-bar-container">
-         <Link to='/' >
-            <img src={backIcon} alt="go back"/>
-         </Link>
-         <img src={logoImg} alt="Proffy"/>
-       </div>
-     </header>
-     <div className="header-content">
-       <strong>These are the available Proffys</strong>
-     </div>
-   </div>
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to='/' >
+            <img src={backIcon} alt="go back" />
+          </Link>
+          <img src={logoImg} alt="Proffy" />
+        </div>
+        <div className="header-content">
+          <strong>These are the available Proffys</strong>
+        </div>
+      </header>
+    </div>
   )
 }
 
