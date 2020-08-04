27b011b3dc30ccaf33d371978717871df54c32fe
leonardo studart <lstudart@pyx4.com>
2020-08-03 20:49:22 -0400

    feat: header component of teacher-list

--- src/pages/teacher-list/index.tsx
+++ src/pages/teacher-list/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import backIcon from '../../assets/icons/back.svg'
+import logoImg from '../../assets/images/logo.svg'
+
+
 
 function TeacherList() {
   return (
-    <h1>teachers</h1>
+   <div id="page-teacher-list" className="container">
+     <header className="page-header">
+       <div className="top-bar-container">
+         <Link to='/' >
+            <img src={backIcon} alt="go back"/>
+         </Link>
+         <img src={logoImg} alt="Proffy"/>
+       </div>
+     </header>
+     <div className="header-content">
+       <strong>These are the available Proffys</strong>
+     </div>
+   </div>
   )
 }
 
