9acfd8f06ddfd8a15dbb52c895652a5ac4d0b97d
leonardo studart <lstudart@pyx4.com>
2020-08-09 17:05:56 -0400

    refactor: replace css by sass in Input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,6 +1,7 @@
-import './styles.css'
+import './styles.scss'
 
 import React, { InputHTMLAttributes } from 'react'
+
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
