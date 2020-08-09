902207724a2e5cf296f650afa7ba2c0d8dd272f7
leonardo studart <lstudart@pyx4.com>
2020-08-09 17:04:49 -0400

    refactor: replace css by sass in PageHeader

--- src/components/page-header/index.tsx
+++ src/components/page-header/index.tsx
@@ -1,12 +1,10 @@
-import './styles.css'
+import './styles.scss'
 
 import React, { PropsWithChildren } from 'react'
 import { Link } from 'react-router-dom'
 
 import backIcon from '../../assets/icons/back.svg'
 import logoImg from '../../assets/images/logo.svg'
-
-
 
 
 
